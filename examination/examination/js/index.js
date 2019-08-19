e0aee49ddf8079ecefe3b02361390a43a31a25a6
wangbing01 <wangbing01@dtinsure.com>
2019-08-19 17:15:23 +0800

    更新体检网站

--- examination/examination/js/index.js
+++
@@ -1,9 +0,0 @@
-$(function () {
-  let url = window.location.href
-  $('.nav li a').each(function () {
-    let dataUrl = $(this).attr('data-url')
-    if (url.indexOf(dataUrl) != -1) {
-      $(this).addClass('change')
-    }
-  })
-})
